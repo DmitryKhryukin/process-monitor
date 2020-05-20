7396af5fdd5b747d94e2592438a9b60c192c3338
Dmitry Khryukin <dmitry.khryukin@gmail.com>
2020-05-20 18:02:32 +0300

    workin with api call // event stream

--- src/API/ProcessMonitor.API/Controllers/ProcessesController.cs
+++ src/API/ProcessMonitor.API/Controllers/ProcessesController.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
 
 namespace ProcessMonitor.API.Controllers
 {
@@ -17,9 +23,26 @@ namespace ProcessMonitor.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetProcessesAsync()
+        public async Task GetProcessesAsync(CancellationToken cancellationToken)
         {
-            return StatusCode(StatusCodes.Status200OK, "processes");
+            string[] data = new string[] {
+                "Hello World!",
+                "Hello Galaxy!",
+                "Hello Universe!"
+            };
+
+            Response.Headers.Add("Content-Type", "text/event-stream");
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+                string dataItem = $"data: {data[i]}\n\n";
+
+                byte[] dataItemBytes = Encoding.ASCII.GetBytes(dataItem);
+                await Response.Body.WriteAsync(dataItemBytes,0,dataItemBytes.Length, cancellationToken);
+                await Response.Body.FlushAsync(cancellationToken);
+            }
+
         }
     }
 }
