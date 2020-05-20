baf2c00a632f21e45836dfb080549e648e781caf
Dmitry Khryukin <dmitry.khryukin@gmail.com>
2020-05-20 19:09:40 +0300

    API // enabled CORS

--- src/API/ProcessMonitor.API/Startup.cs
+++ src/API/ProcessMonitor.API/Startup.cs
@@ -20,6 +20,8 @@ namespace ProcessMonitor.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             services.AddScoped<IProcessService, ProcessService>();
 
             services.AddControllers();
@@ -32,6 +34,11 @@ namespace ProcessMonitor.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
