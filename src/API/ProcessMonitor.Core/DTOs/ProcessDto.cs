5f67cc46d896067b5b0afe0c7403e21d9731697e
Dmitry Khryukin <dmitry.khryukin@gmail.com>
2020-05-20 23:12:06 +0300

    ProcessService has been updated // bug report

--- src/API/ProcessMonitor.Core/DTOs/ProcessDto.cs
+++ src/API/ProcessMonitor.Core/DTOs/ProcessDto.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace ProcessMonitor.Core.DTOs
 {
     public class ProcessDto
     {
-        public string Name { get; set; }
+        public int Id { get; set; }
+        public string ProcessName { get; set; }
+        public double CpuTime { get; set; }
+        public int ThreadsCount { get; set; }
     }
 }
--- src/API/ProcessMonitor.Core/Services/ProcessService.cs
+++ src/API/ProcessMonitor.Core/Services/ProcessService.cs
@@ -8,14 +8,25 @@ namespace ProcessMonitor.Core.Services
 {
     public class ProcessService : IProcessService
     {
+        /// <summary>
+        /// it's a bug related to TotalProcessorTime, so can't use it for now
+        /// https://github.com/dotnet/runtime/issues/36777
+        /// https://stackoverflow.com/questions/61921540/process-prototalprocessortime-throws-invalidoperationexception-exception-on-maco
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<ProcessDto> GetCurrentProcesses()
         {
             var processes = Process.GetProcesses();
 
-            return processes.Select(x => new ProcessDto()
+            return processes
+                .Select(x => new ProcessDto()
             {
-                Name = x.ProcessName
-            });
+                Id = x.Id,
+                ProcessName = x.ProcessName,
+                ThreadsCount = x.Threads.Count,
+                // it's a bug related to TotalProcessorTime, so can't use it for now - check link above
+                 CpuTime = 1000 //x.TotalProcessorTime.TotalMilliseconds
+            }).ToList();
         }
     }
 }
