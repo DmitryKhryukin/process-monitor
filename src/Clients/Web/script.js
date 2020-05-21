cce790632914263f5e0634c922a3569f059a9ac3
Dmitry Khryukin <dmitry.khryukin@gmail.com>
2020-05-21 19:55:34 +0300

    frontend client has been updated

--- src/Clients/Web/script.js
+++ src/Clients/Web/script.js
@@ -4,25 +4,77 @@ let eventSource = new EventSource('https://localhost:5001/api/processes');
 let loadingDiv = document.getElementById("loading");
 let processesInfoList = document.getElementById("processesInfo");
 
-eventSource.addEventListener("open", function(e) { 
-    loadingDiv.hidden = false;
+eventSource.addEventListener("open", function (e) {
+  loadingDiv.hidden = false;
 });
 
-eventSource.addEventListener("message", function(e) {
+eventSource.addEventListener("message", function (e) {
+
+  loadingDiv.hidden = true;
+  processesInfoList.textContent = '';
+
+  var data = JSON.parse(e.data);
+  let processTable = generateProcessTable(data);
+  processesInfo.appendChild(processTable);
+});
+
+eventSource.addEventListener("error", function (e) {
+  console.log("error");
+  console.log(JSON.stringify(e));
+  console.log(e.data);
+});
+
+function generateProcessTable(processes) {
+
+  if (processes.length > 0) {
+
+    let tbl = document.createElement("table");
+    let tblBody = document.createElement("tbody");
+
+    let header = document.createElement("thead");
+    let headerRow = document.createElement("tr");
     
-    loadingDiv.hidden = true;
-    processesInfoList.textContent = '';
+    
+    for(const property in processes[0]){
+      let cell = document.createElement("th");
+      let cellText = document.createTextNode(property);
+      cell.appendChild(cellText);
+      headerRow.appendChild(cell);
+    }
 
-    var data = JSON.parse(e.data);
-    data.forEach(function(item, index) {
-        var elem = document.createElement("li");
-        elem.innerHTML = "<div>"+item.ProcessName+"</div>";
-        processesInfoList.appendChild(elem);
-    });
-  });
+    header.appendChild(headerRow);
+    tblBody.appendChild(headerRow);
 
-eventSource.addEventListener("error", function(e) {
-    console.log("error");
-    console.log(JSON.stringify(e));
-    console.log(e.data);
-  });
+    for (var i = 0; i < processes.length; i++) {
+
+      var row = document.createElement("tr");
+
+      for (const property in processes[i]) {
+
+        let cell = document.createElement("td");
+        let cellText = document.createTextNode(processes[i][property]);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      }
+
+      tblBody.appendChild(row);
+    }
+
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+
+    // sets the border attribute of tbl to 2;
+    tbl.setAttribute("border", "2");
+
+    return tbl;
+  } else {
+    return document.createElement("div").innerHTML("No detaild information about processes");
+  }
+}
+
+function getProcessRowHtml(process) {
+
+
+  "<div>" + item.ProcessName + "</div>"
+
+}
